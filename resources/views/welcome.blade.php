be0989f9711c2006729ef1e9bd679d0b8a5daa5a
projetoengenhariasaude <projetoengenhariasaude@outlook.com>
2022-10-26 01:06:03 -0300

    gif

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,21 @@
 <html>
 
+<style>
+    .gif-center{
+        text-align: center;
+    }
+</style>
 
 
+<body>
+    <div class="gif-center">
+        <h1>I will be a</h1>
+        <img src="https://media4.giphy.com/media/3knKct3fGqxhK/giphy.gif" alt="" srcset="" width="500px" height="300px">
 
+    </div>
+    
+</body>
 
-<h1>aeeeeeeeeeeeee</h1>
 
 
 
